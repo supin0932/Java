a249f0d893903109c0ca9e7eba074ae277e0a722
nhut.le <nhut.le@mservice.com.vn>
2025-06-26 16:46:03 +0700

    update

--- src/bai1.java
+++ src/bai1.java
@@ -2,7 +2,6 @@ public class bai1 {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println(1234567890);
-        System.out.println(1234567890);
 
     }
 }
