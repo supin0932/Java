643b8977a1966f3c78f68a75cf069499c98584fd
nhut.le <nhut.le@mservice.com.vn>
2025-07-01 11:25:18 +0700

    update

---
+++ src/bai5.java
@@ -0,0 +1,22 @@
+public class bai5 {
+    public static void main(String[] args) {
+        //khai báo biến
+        int a;
+        byte tuoi;
+        float diemToan;
+        //khai báo nhiều biến 1 lúc cùng kiểu
+        int c,d,e,f;
+
+        //khởi tạo biến
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 7.5;
+        System.out.println(tuoiCon);
+        tuoiCon=15;
+        System.out.println(tuoiCon);
+
+        //hằng số
+        final int DOISO=100;
+        int tuoicon = 30;
+    }
+}
