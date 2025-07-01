9866318e29fcf72f6470beea4428b061bf116ea9
nhut.le <nhut.le@mservice.com.vn>
2025-07-01 11:48:51 +0700

    update

---
+++ src/bai6.java
@@ -0,0 +1,19 @@
+public class bai6 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        double kq = (double) a/b; // int >> double
+        System.out.println(kq);
+
+        //ép kiểu hẹp
+        int c = 128;
+        byte d = (byte) c;
+        System.out.println(c);
+        System.out.println(d);
+
+        int e = 15;
+        byte f = (byte) e; // ép từ kiểu int >> byte
+        System.out.println(e);
+        System.out.println(f);
+        }
+    }
