1710f6e455229c698b2f5aec89e2a7fe4e03ad10
nhut.le <nhut.le@mservice.com.vn>
2025-07-01 10:58:44 +0700

    update

--- src/bai2.java
+++ src/bai2.java
@@ -1,2 +1,21 @@
+import java.sql.SQLOutput;
+
 public class bai2 {
+    public static void main(String[] args) {
+        System.out.println("dòng 1"); //sout
+        System.out.println("dòng 2"); //sout
+        System.out.println("dòng 3"); //ctrl + D
+
+        //print
+        System.out.print("dòng 4");
+        System.out.print("dòng 5");
+        System.out.println(" ");
+
+        // \t
+        System.out.println("Hello work");
+        System.out.println("\tLê Minh Nhựt");
+
+        // \n
+        System.out.println("Cuộc sống\n Vui vẻ");
+    }
 }
