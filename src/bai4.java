764355b00ec334f170e842634f354e0445fff019
nhut.le <nhut.le@mservice.com.vn>
2025-07-01 11:12:54 +0700

    update

---
+++ src/bai4.java
@@ -0,0 +1,11 @@
+public class bai4 {
+    public static void main(String[] args) {
+        // Kiểm tra kiểu nguyên
+        System.out.println("max của kiểu int" +Integer.MAX_VALUE);
+        System.out.println("max của kiểu int" +Integer.MIN_VALUE);
+
+        // Kiểm tra kiểu byte
+        System.out.println("max của kiểu byte"+Byte.MAX_VALUE);
+        System.out.println("max của kiểu byte"+Byte.MIN_VALUE);
+    }
+}
