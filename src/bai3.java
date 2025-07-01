156a143383ca9e2cf746d7145587b5be251a803e
nhut.le <nhut.le@mservice.com.vn>
2025-07-01 11:04:44 +0700

    update

--- src/bai3.java
+++ src/bai3.java
@@ -1,2 +1,12 @@
 public class bai3 {
+    public static void main(String[] args) {
+        // đây là ghi chú 1 dòng
+        System.out.println("dòng 1");
+        System.out.println("dòng 2"); // ctrl +/
+        System.out.println("dòng 3");
+
+        /* đây là
+        ghi chú nhiều dòng
+        */
+    }
 }
